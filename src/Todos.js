ce4b0f935a3e2c4608078a63ee98541483681228
celiaphung <celiaphung@gmail.com>
2020-08-05 20:34:06 -0400

    WIP: work

---
+++ src/Todos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import List from './List';
+
+class Todos extends React.Component {
+    render() {
+        return (
+            <div>
+                <List />
+            </div>
+        )
+    }
+}
+
+export default Todos
